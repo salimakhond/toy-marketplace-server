a1d4e832cf7d72df93abb781605e43ec7c630fb2
salimakhond <salimakhond@gmai.com>
2023-05-20 04:59:03 +0600

    put method added

--- index.js
+++ index.js
@@ -70,7 +70,10 @@ async function run() {
         });
 
 
-        
+        app.put('/product-by-id/:id',async(req,res)=>{
+            const id=req.params.id;
+            console.log(id)
+        });
 
 
         app.get('/products', async (req, res) => {
